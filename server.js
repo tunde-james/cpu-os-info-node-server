302b66094e1fa3a808cf8d5a1c03f18ce57fd2bf
Tunde James <tundejames04@gmail.com>
2024-05-10 15:53:43 +0100

    Read from a file using the fs module

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require('node:http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log({ url: req.url, method: req.method, body: 'Hello' });
+  if (req.url === '/jokes') {
+    fs.readFile('./db.json', 'utf-8', (err, data) => {
+      if (err) throw err;
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+      res.end(data);
+    });
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
